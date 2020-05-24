5e6808b84e2fb19b69032f0680e948eabead81e3
Simon Scatton <simon.scatton@epita.fr>
2020-05-24 05:33:49 +0200

    Added on click zoom to the preview

--- mandelbrot.py
+++ mandelbrot.py
@@ -3,6 +3,7 @@ from math import *
 from cmath import *
 import time
 
+
 class Mandelbrot:
 
     def __init__(self, window, color, size, coords):
@@ -12,9 +13,9 @@ class Mandelbrot:
         # --------------------
         self.color = color
         self.max_iteration = 50
-    
+
     # --------------------------------------------------------------
-        
+
     def draw(self):
         n = 0
         progression = 0
@@ -22,26 +23,27 @@ class Mandelbrot:
 
         for x in range(self.width):
             for y in range(self.height):
-                z = 0
                 i = 0
-                n+=1
-                
-                if n % ((self.width*self.height)//100) == 0:
+                n += 1
+                if n % ((self.width * self.height) // 100) == 0:
                     progression += 1
                     print(f"Loading... {progression}%")
-                
-                cx = self.xmin + self.xmax * x / self.width
-                cy = self.ymin + self.ymax * y / self.height
-                c = complex(cx,cy)
-
-                while abs(z) < 2 and i < self.max_iteration:
-                    z = z**2 + c
-                    i+=1
-
+                # Transforming the size of the box to fit the complex plane
+                cx = x * (self.xmax - self.xmin) / self.width + self.xmin
+                cy = y * (self.ymax - self.ymin) / self.width + self.ymin
+                xn = 0
+                yn = 0
+                while (xn * xn + yn * yn) < 4 and i < self.max_iteration:
+                    # Evaluating the value of next sequence term
+                    tmp_x = xn
+                    tmp_y = yn
+                    xn = tmp_x * tmp_x - tmp_y * tmp_y + cx
+                    yn = 2 * tmp_x * tmp_y + cy
+                    i += 1
                 if i == self.max_iteration:
-                    self.window.set_at((x,y), self.color)
+                    self.window.set_at((x, y), self.color)
                 else:
-                    self.window.set_at((x,y), (i*255/self.max_iteration, i*255/self.max_iteration, i*255/self.max_iteration))
-                    
-        
+                    self.window.set_at((x, y), (
+                        i * 255 / self.max_iteration, i * 255 / self.max_iteration, i * 255 / self.max_iteration))
+
         print(f"Mandelbrot fractal successfully generated in {round(time.time() - time_, 2)}s")
