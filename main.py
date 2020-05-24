e7df3f3a28612e2b49025e55d009f076a18fb503
mortim1 <karim_aoulad@outlook.fr>
2020-05-24 08:48:16 +0200

    :art: Improve structure

--- main.py
+++ main.py
@@ -2,20 +2,7 @@ import pygame
 import math
 from mandelbrot import Mandelbrot
 
-PARAMS_ = {"color": (0, 0, 0), "size": (600, 600), "coords": (-2, 0.5, -1.25, 1.25)}
-
-# Zoom factor for each click
-zoomfactor = 2
-
-
-# Util functions---------------------------------------------
-def interpolate(start, end, interpolation):
-    return start + ((end - start) * interpolation)
-
-
-def complex_transform(grid_position, height, max, min):
-    return grid_position / (height / (max - min)) + min
-
+PARAMS_ = {"color": (0, 0, 0), "backgroundColor": (0, 0, 255), "size": (600, 600), "coords": (-2, 0.5, -1.25, 1.25), "zoomfactor":2}
 
 # -----------------------------------------------------------
 
@@ -26,12 +13,13 @@ pygame.display.set_caption("Mandelbrot")
 window = pygame.display.set_mode(PARAMS_["size"])
 size = pygame.display.get_surface().get_size()
 
-mandelbrot = Mandelbrot(window, PARAMS_["color"], PARAMS_["size"], PARAMS_["coords"])
+mandelbrot = Mandelbrot(window, PARAMS_["color"], PARAMS_["backgroundColor"], PARAMS_["size"], PARAMS_["coords"], PARAMS_["zoomfactor"])
 mandelbrot.draw()
 
 pygame.display.flip()
 
-# Events
+# -----------------------------------------------------------
+
 isloop = True
 while isloop:
     for event in pygame.event.get():
@@ -39,24 +27,28 @@ while isloop:
         if event.type == pygame.QUIT:
             isloop = False
         elif event.type == pygame.MOUSEBUTTONDOWN:
+
             if event.button == 1:
+
+                # -----------------------
                 p = pygame.mouse.get_pos()
                 # Transforming pygame coordinates to complex plan values
-                mouseRealPart = complex_transform(p[0], mandelbrot.width, mandelbrot.xmax, mandelbrot.xmin)
-                mouseImaginaryPart = complex_transform(p[1], mandelbrot.width, mandelbrot.ymax, mandelbrot.xmin)
+                mouseRealPart = mandelbrot.complex_transform(p[0], mandelbrot.width, mandelbrot.xmax, mandelbrot.xmin)
+                mouseImaginaryPart = mandelbrot.complex_transform(p[1], mandelbrot.height, mandelbrot.ymax, mandelbrot.ymin)
                 # Calculating the interpolation/zoom value
-                interpolation = 1.0 / zoomfactor
+                interpolation = 1.0 / mandelbrot.zoomfactor
                 # Redefining the values of the initial box drawing Moving the square of calculation around the mouse
                 # click coordinates with the interpolate method and "zooming"
-                mandelbrot.xmin = interpolate(mouseRealPart, mandelbrot.xmin, interpolation)
-                mandelbrot.ymin = interpolate(mouseImaginaryPart, mandelbrot.ymin, interpolation)
-                mandelbrot.xmax = interpolate(mouseRealPart, mandelbrot.xmax, interpolation)
-                mandelbrot.ymax = interpolate(mouseImaginaryPart, mandelbrot.ymax, interpolation)
-                # Recalling the draw function
+                mandelbrot.xmin = mandelbrot.interpolate(mouseRealPart, mandelbrot.xmin, interpolation)
+                mandelbrot.ymin = mandelbrot.interpolate(mouseImaginaryPart, mandelbrot.ymin, interpolation)
+                mandelbrot.xmax = mandelbrot.interpolate(mouseRealPart, mandelbrot.xmax, interpolation)
+                mandelbrot.ymax = mandelbrot.interpolate(mouseImaginaryPart, mandelbrot.ymax, interpolation)
+                # -----------------------
+
                 mandelbrot.draw()
+
             elif event.button == 2:
                 p = pygame.mouse.get_pos()
-                print(p)
-                #TODO Add a "unzoom function" for instance save x and y s in a list and recusively redraw previous zooms.
+            
 
         pygame.display.flip()
--- mandelbrot.py
+++ mandelbrot.py
@@ -6,10 +6,12 @@ import time
 
 class Mandelbrot:
 
-    def __init__(self, window, color, size, coords):
+    def __init__(self, window, color, backgroundColor, size, coords, zoomfactor):
         self.window = window
         self.width, self.height = size
         self.xmin, self.xmax, self.ymin, self.ymax = coords
+        self.zoomfactor = zoomfactor
+        self.backgroundColor = backgroundColor
         # --------------------
         self.color = color
         self.max_iteration = 50
@@ -25,25 +27,40 @@ class Mandelbrot:
             for y in range(self.height):
                 i = 0
                 n += 1
+
                 if n % ((self.width * self.height) // 100) == 0:
                     progression += 1
                     print(f"Loading... {progression}%")
+
                 # Transforming the size of the box to fit the complex plane
                 cx = x * (self.xmax - self.xmin) / self.width + self.xmin
                 cy = y * (self.ymax - self.ymin) / self.width + self.ymin
                 xn = 0
                 yn = 0
-                while (xn * xn + yn * yn) < 4 and i < self.max_iteration:
-                    # Evaluating the value of next sequence term
+
+                while (xn**2 + yn**2) < 4 and i < self.max_iteration:
                     tmp_x = xn
                     tmp_y = yn
-                    xn = tmp_x * tmp_x - tmp_y * tmp_y + cx
+                    xn = tmp_x**2 - tmp_y**2 + cx
                     yn = 2 * tmp_x * tmp_y + cy
                     i += 1
+
                 if i == self.max_iteration:
                     self.window.set_at((x, y), self.color)
                 else:
                     self.window.set_at((x, y), (
-                        i * 255 / self.max_iteration, i * 255 / self.max_iteration, i * 255 / self.max_iteration))
+                        i * self.backgroundColor[0] / self.max_iteration, i * self.backgroundColor[1] / self.max_iteration, i * self.backgroundColor[2] / self.max_iteration)
+                    )
 
         print(f"Mandelbrot fractal successfully generated in {round(time.time() - time_, 2)}s")
+
+
+    # ---------- Util functions -----------------------------------
+
+    def complex_transform(self, grid_position, size, max, min):
+        return grid_position / (size / (max - min)) + min
+
+    def interpolate(self, start, end, interpolation):
+        return start + ((end - start) * interpolation)
+
+    # --------------------------------------------------------------
